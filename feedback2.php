e79d052a1cb7ccf0a295047d4ab71e4c9827d0b7
swedelviola <59990454+swedelviola@users.noreply.github.com>
2020-02-09 18:02:47 +0530

    admin page to view feedback

--- feedback2.php
+++ feedback2.php
@@ -1,14 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '');
-if (!$conn){
-    die("Database Connection Failed" . mysqli_error($conn));
-}
-$select_db = mysqli_select_db($conn, 'crazepgm');
-if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($conn));
-} 
-
-
+require 'connection.php';
 
 $name = $_GET['feed'];
 
