f5155a3e7aa2e28163d8bbb258c8fc0248caa086
swedelviola <59990454+swedelviola@users.noreply.github.com>
2020-02-09 15:12:10 +0530

    proper admin

---
+++ addcomp.php
@@ -0,0 +1,81 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '');
+if (!$conn){
+    die("Database Connection Failed" . mysqli_error($conn));
+}
+$select_db = mysqli_select_db($conn, 'crazepgm');
+if (!$select_db){
+    die("Database Selection Failed" . mysqli_error($conn));
+}
+session_start();
+$_SESSION['default']=0;
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="file:///home/shru/Downloads/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+  <script type="text/javascript" src="file:///home/shru/Downloads/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+  
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="Dashboard.css">
+  <title>Add Competition</title>
+</head>
+<body>
+
+	<nav class="navbar navbar-dark navbar-expand-sm fixed-top" style="background:#232f34">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#" style="color:#ffa852">Crazy Programming <br><h3 >Admin</h3> </a>
+      <li>
+        <a onClick="logout()" id="log" href="Login.html" class="navbar-brand pull-right" style="color:#F0F0F0"  >Logout</a>
+    </li>
+    </div>
+  </nav>  
+
+
+  <div class="sidenav">
+    <br><br>
+  <a href="Dashboard.php" >Leaderboard</a><br>
+  <a href="event.php" >Events</a><br>
+  <a href="addcomp.php" id="hell" active>Add Competition</a><br>
+  <a href="changewei.php">Change weightage</a><br>
+  <a href="checkwei.php">Current weightage</a><br>
+  <a href="feedback.php">Feedback</a><br>
+</div>
+<br>
+<br>
+<div class="main">
+  <div class="row" id="contain-card">
+      <div class="col-md-8">
+        <div class="card " style="margin:20px 15px 20px 15px">
+          <div class="card-header text-right" style="background:#ffa852;">
+          <b>New competition</b>
+          </div>
+          <div class="card-body">
+            <h4 class="ui-title text-sec-headline-xs">Select Competition type:</h4><br>
+            <form action="addcomp2.php">
+                  <div class="form-check">
+                  <input type="radio" class="form-check-input" name="radio" >
+                  <label class="form-check-label" for="Event name" required>Crazy Programming</label><br><br>
+                  <button type="submit" class="btn btn-primary" formaction="addcomp2.php">Next</button>
+
+
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+
+</div>
+</body>
+</html>
